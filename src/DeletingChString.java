94f9a12b6e8f3c5ff29c8483b5590b92d770d382
Adrian <adriancerva2021@gmail.com>
2020-10-12 22:03:18 -0500

    update

--- src/DeletingChString.java
+++ src/DeletingChString.java
@@ -9,22 +9,37 @@
 import acm.program.*;
 
 public class DeletingChString extends ConsoleProgram {
-	public void run() {
-	  println()
-	}
-	
-	
-	public String removeAllOccurrences(String str, char ch) {
-	
-		String str = 0;
-		for(int i = 0; i<str.length(); i++) {
-			for(int j = 0; j <= 0; i++) {
-				
-			}
-		}
+
+		
+	   public void run(){
+	        String output1 = removeAllOccurrences("This is a test", 't');
+	        println(output1);
+	        String output2 = removeAllOccurrences("Summer is here", 'e');
+	        println(output2);
+	        String output3 = removeAllOccurrences("---0---", '-');
+	        println(output3);
+	    }
+
+	    private String removeAllOccurrences(String str, char c) {
+
+	        char[] strArray = str.toCharArray();
+	        char[] outputArray = new char[strArray.length];
+	        int outputArrayIndex = 0;
+	        for (int i = 0; i < strArray.length; i++) {
+	            char s = strArray[i];
+	            if (s != c) {
+	                outputArray[outputArrayIndex] = s;
+	                outputArrayIndex++;
+	            }
+
+	        }
+	        return new String(outputArray, 0, outputArrayIndex);
+
+	    }
+		
 	}
 		
 		
-	}
+	
 
 
